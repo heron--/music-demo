40114fca24f81c93a902a6f41fef14a97fdc3bed
Devin Marsh <devin.marsh@sharecare.com>
2017-09-20 16:44:34 -0400

    Make that audio controller make some noise

--- src/audioController.js
+++ src/audioController.js
@@ -4,9 +4,10 @@ import { incrementCurrentNote as createIncNoteAction } from './actions';
 function AudioController(store) {
     this.store = store;
     this.unsubscribe = this.store.subscribe(this.handleStateChange.bind(this));
-    this.timeoutId = null;
     this.playing = false;
     this.lastStep = null;
+    this.tracks = [];
+    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
 }
 
 AudioController.prototype.handleStateChange = function handleStateChange() {
@@ -16,15 +17,27 @@ AudioController.prototype.handleStateChange = function handleStateChange() {
         playstate 
     } = currentState.controls
 
+    const currentTrack = currentState.trackControl.tracks.filter(t => t.id === currentState.trackControl.currentTrack)[0];
+
     if(playstate === 'PLAYING') {
         if(!this.playing) {
             this.step();
         }
-    } else {
-        if(this.timeoutId !== null) {
-            window.clearTimeout(this.timeoutId);
+    }
+
+    if(typeof currentTrack !== 'undefined') {
+        if(this.tracks.filter(t => t.id  === currentTrack.id).length === 0) {
+            this.initTrack(currentTrack);
+        } else {
+            const localTrack = this.tracks.filter(t => t.id === currentTrack.id)[0];
+
+            localTrack.noteLanes.forEach((nl,i) => {
+                if(nl.notes !== currentTrack.noteLanes[i].notes) {
+                    nl.notes = currentTrack.noteLanes[i].notes;
+                }
+            })
         }
-    } 
+    }
 };
 
 AudioController.prototype.step = function step() {
@@ -47,6 +60,7 @@ AudioController.prototype.step = function step() {
         window.requestAnimationFrame(this.step.bind(this));
     } else {
         this.incrementCurrentNote();
+        this.playNotes();
         this.lastStep = window.Date.now();
         window.requestAnimationFrame(this.step.bind(this));
     }
@@ -55,5 +69,59 @@ AudioController.prototype.step = function step() {
 AudioController.prototype.incrementCurrentNote = function incrementCurrentNote() {
     this.store.dispatch(createIncNoteAction());
 };
+
+AudioController.prototype.playNotes = function playNotes() {
+    const currentState = this.store.getState();
+
+    const currentTrack = this.tracks.filter(t => t.id === currentState.trackControl.currentTrack)[0];
+    const notePosition = currentState.trackControl.notePosition;
+
+    currentTrack.noteLanes.forEach(nl => {
+        if(nl.notes[notePosition].active) {
+            nl.gain.gain.value = currentState.controls.volume
+
+            if(!nl.notes[notePosition].sustain) {
+                window.setTimeout(() => {
+                    nl.gain.gain.value = 0;
+                }, 100) // Make this not hard coded later
+            }
+        } else {
+            nl.gain.gain.value = 0;
+            window.clearTimeout(nl.timeoutId);
+        }
+    });
+};
+
+AudioController.prototype.initTrack = function initTrack(track){
+    let trackObject = {...track};
+
+    const instrumentLookup = {
+        [CONST.INSTRUMENTS.SQUARE.name]: 'square',
+        [CONST.INSTRUMENTS.TRIANGLE.name]: 'triangle',
+        [CONST.INSTRUMENTS.SAWTOOTH.name]: 'sawtooth',
+        [CONST.INSTRUMENTS.PULSE.name]: 'custom',
+        [CONST.INSTRUMENTS.NOISE.name]: 'custom'
+    };
+
+    trackObject.noteLanes = trackObject.noteLanes.map(nl => {
+        return {
+            ...nl,
+            oscillator: this.audioContext.createOscillator(),
+            gain: this.audioContext.createGain(),
+            timeoutId: null
+        }
+    });
+
+    trackObject.noteLanes.forEach(nl => {
+        nl.oscillator.type = instrumentLookup[track.instrument];
+        nl.oscillator.frequency.value = nl.value;
+        nl.gain.gain.value = 0;
+        nl.oscillator.connect(nl.gain);
+        nl.gain.connect(this.audioContext.destination);
+        nl.oscillator.start();
+    });
+
+    this.tracks.push(trackObject)
+}
 
 export default AudioController;
