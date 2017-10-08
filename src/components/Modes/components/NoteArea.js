ce3ae0bff5a3e753361902b922e978dcc35a638c
Devin Marsh <devin.marsh@sharecare.com>
2017-10-08 11:28:29 -0400

    Add componentShouldUpdate performance enhancement for Note Lanes

--- src/components/Modes/components/NoteArea.js
+++ src/components/Modes/components/NoteArea.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import CONST from '../../../constants';
 
@@ -23,66 +23,70 @@ const NoteArea = ({
     );
 };
 
-const NoteLane = ({
-    notePosition,
-    trackId,
-    value,
-    name,
-    octave,
-    notes,
-    cycleNote
-}) => {
-
-
-    const octaveColor = CONST.COLORS[CONST.OCTAVE_COLORS[octave]];
-
-    const octaveStyle = {
-        color: octaveColor.cssColor
+class NoteLane extends Component {
+    shouldComponentUpdate(nextProps) {
+        return !(nextProps.notes === this.props.notes) || !(nextProps.notePosition === this.props.notePosition);
     }
 
-    return (
-        <div className="note-lane">
-            <div className="note-lane__label"><span style={ octaveStyle }>{octave}</span> { name }</div>
-            <div className="notes">
-                {
-                    // Render notes
-                    // Seems to perform better when they're not components
-                    notes.map((n, i) => {
+    render() {
+        const {
+            notePosition,
+            trackId,
+            name,
+            octave,
+            notes,
+            cycleNote
+        } = this.props;
 
-                        const noteClasses = classNames({
-                            'note': true,
-                            'note--active': n.active,
-                            'note--sustain': n.sustain,
-                            'note--beat': i % 4 === 0,
-                            'note--current': i === notePosition
-                        });
+        const octaveColor = CONST.COLORS[CONST.OCTAVE_COLORS[octave]];
 
-                        return (
-                            <div key={ i } className={ noteClasses } onClick={() => { cycleNote(trackId, name, octave, i) }}></div>
-                        );
+        const octaveStyle = {
+            color: octaveColor.cssColor
+        }
 
-                    })
-                }
-                {
-                    // Render "phony" notes
-                    [...new Array(16)].map((pn, i) => {
-                        const noteClasses = classNames({
-                            'note': true,
-                            'note--phony': true,
-                            'note--beat': i % 4 === 0
-                        });
+        return (
+            <div className="note-lane">
+                <div className="note-lane__label"><span style={ octaveStyle }>{octave}</span> { name }</div>
+                <div className="notes">
+                    {
+                        // Render notes
+                        // Seems to perform better when they're not components
+                        notes.map((n, i) => {
 
-                        return (
-                            <div key={ i } className={ noteClasses }>
-                                <div></div>
-                            </div>
-                        ); 
-                    })
-                }
+                            const noteClasses = classNames({
+                                'note': true,
+                                'note--active': n.active,
+                                'note--sustain': n.sustain,
+                                'note--beat': i % 4 === 0,
+                                'note--current': i === notePosition
+                            });
+
+                            return (
+                                <div key={ i } className={ noteClasses } onClick={() => { cycleNote(trackId, name, octave, i) }}></div>
+                            );
+
+                        })
+                    }
+                    {
+                        // Render "phony" notes
+                        [...new Array(16)].map((pn, i) => {
+                            const noteClasses = classNames({
+                                'note': true,
+                                'note--phony': true,
+                                'note--beat': i % 4 === 0
+                            });
+
+                            return (
+                                <div key={ i } className={ noteClasses }>
+                                    <div></div>
+                                </div>
+                            ); 
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    );
-};
-
+        );
+    } 
+}
 
 export { NoteArea as default };
