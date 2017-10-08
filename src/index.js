70d76ab1fd40b3dbd109f2a7812d631415809739
Devin Marsh <devin.marsh@sharecare.com>
2017-10-08 16:56:03 -0400

    Prep for first hosting

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import { store } from './store';
 import App from './components/App';
 import AudioController from './audioController';
 import registerServiceWorker from './registerServiceWorker';
+import ua from 'universal-analytics';
+
+const visitor = ua('UA-107756414-1');
+
+if(process.env.NODE_ENV === 'production') {
+    visitor.pageview('/').send();
+}
 
 new AudioController(store);
 
